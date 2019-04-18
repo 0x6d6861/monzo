d2e5da00ed9d67de127f3ae4fd187b1448419f39
Heri Agape <agape@live.fr>
2019-04-18 15:14:39 +0300

    added registration

--- app/src/main/java/co/heri/monzo/Authentication/RegisterActivity.kt
+++ app/src/main/java/co/heri/monzo/Authentication/RegisterActivity.kt
@@ -10,10 +10,15 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_register.*
 import java.util.regex.Pattern
 import android.widget.EditText
-
+import android.widget.CompoundButton
+import android.widget.RadioGroup
+import co.heri.monzo.MainActivity
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.UserProfileChangeRequest
 
 
 class RegisterActivity : AppCompatActivity() {
+
 
     private lateinit var mAuth: FirebaseAuth
 
@@ -24,7 +29,90 @@ class RegisterActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_register)
 
-        login_link.setOnClickListener {
+        fullname_input.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(!fullname_input.text.isNullOrBlank()){
+                    fullname_input_layout.error = null
+                } else {
+                    fullname_input_layout.error = "Please provide your full name"
+                }
+            }
+
+        })
+
+        password_input.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(!password_input.text.isNullOrBlank()){
+                    password_input_layout.error = null
+                } else {
+                    password_input_layout.error = "Please a password for your account"
+                }
+            }
+
+        })
+
+
+        email_input.addTextChangedListener(object: TextWatcher  {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if(!email_input.text.isNullOrBlank()){
+                    email_input_layout.error = null
+                } else {
+                    email_input_layout.error = "Email Address is required"
+                }
+                Is_Valid_Email(email_input);
+            }
+
+            fun Is_Valid_Email(edt: EditText) {
+                if (edt.text.toString() == null) {
+                    email_input.error = "Invalid Email Address"
+//                        valid_email = null
+                } else if (isEmailValid(edt.text.toString()) == false) {
+                    email_input.error = "Invalid Email Address"
+//                        valid_email = null
+                } else {
+//                        valid_email = edt.text.toString()
+                    email_input.error = null
+                }
+            }
+
+            fun isEmailValid(email: CharSequence): Boolean {
+                return android.util.Patterns.EMAIL_ADDRESS.matcher(email)
+                    .matches()
+            }
+
+        })
+
+        confirm_check.setOnCheckedChangeListener { buttonView, isChecked ->
+            if (isChecked) {
+                confirm_check.error = null
+            } else {
+                confirm_check.error = "Confirm terms of service"
+            }
+        }
+
+        register_btn.setOnClickListener {
 
             val fullname = fullname_input.text.toString()
             val email = email_input.text.toString()
@@ -33,39 +121,6 @@ class RegisterActivity : AppCompatActivity() {
             val phone = phone_input.text.toString() // TODO: input validation
 
 
-            email_input.addTextChangedListener(object: TextWatcher  {
-                override fun afterTextChanged(s: Editable?) {
-
-                    Is_Valid_Email(email_input);
-
-                }
-
-                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-                }
-
-                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-                }
-
-                fun Is_Valid_Email(edt: EditText) {
-                    if (edt.text.toString() == null) {
-                        email_input_layout.error = "Invalid Email Address"
-//                        valid_email = null
-                    } else if (isEmailValid(edt.text.toString()) == false) {
-                        email_input_layout.error = "Invalid Email Address"
-//                        valid_email = null
-                    } else {
-//                        valid_email = edt.text.toString()
-                    }
-                }
-
-                fun isEmailValid(email: CharSequence): Boolean {
-                    return android.util.Patterns.EMAIL_ADDRESS.matcher(email)
-                        .matches()
-                }
-
-            })
 
 
             if(fullname.isNullOrBlank()){
@@ -88,13 +143,27 @@ class RegisterActivity : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-            if (password !== passwordConfirm){
+            if (password.compareTo(passwordConfirm) != 0 ){
                 password_input_repeat.error = "Passwords provided do not match"
                 return@setOnClickListener
             }
 
+            if(!confirm_check.isChecked){
+                confirm_check.error = "You need to agree to the terms of service"
+            }
 
-            startActivity(Intent(this, LoginActivity::class.java))
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if(it.isSuccessful){
+                    val user = mAuth.currentUser
+                    val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(fullname).build()
+                    user!!.updateProfile(profileUpdates)
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Snackbar.make(register_btn, "Registration failed, try later", Snackbar.LENGTH_SHORT).show()
+                }
+            }
         }
     }
+
+
 }
